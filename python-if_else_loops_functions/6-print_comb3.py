5fac6cdb1558f9882731235761110fb3b5d60094
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-01 18:02:04 -0400

    using format print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(10, 99):
-    for z in range(0, 9):
-        for k in range(0, 9):
-            if (i != z and z != k and i != k):
-                print(i, )
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i >= j:
+            pass
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
