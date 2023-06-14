803fe418925a7a25df92618220ad110c50de710b
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-14 14:58:59 -0400

    private instance

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Module containing the the method init."""
+
+
+class Square:
+
+    """Class containing a private instance."""
+
+    def __init__(self, size):
+        """Method containing the size of square"""
+
+        self.__size = size
