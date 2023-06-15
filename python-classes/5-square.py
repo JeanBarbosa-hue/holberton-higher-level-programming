6ca9abccbe7226911a8466e738ee8edfa8b2ca30
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-15 15:56:09 -0400

    print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Contains class called square."""
+
+
+class Square:
+    """"""
+
+    def __init__(self, size=0):
+        self.size = size
+        """"""
+
+    def size(self):
+        """"""
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """"""
+        if self.size == 0:
+            print("")
+        else:
+            for row in range(self.size):
+                print("#" * self.size)
