48f3c09d64e9495208645e385edc21d03ce81626
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-15 10:49:58 -0400

    propert setter and getter

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Contains class called square."""
+
+
+class Square:
+    def __init__(self, size=0):
+        """This is a constructor method."""
+        self.size = size
+
+    @property
+    def size(self):
+        """This is a property method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This is property setter method."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate square area method."""
+        return self.__size ** 2
