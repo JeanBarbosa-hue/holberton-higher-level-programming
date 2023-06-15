89b3dc1c465c662db3a50d0015ac70b452441019
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-15 15:59:41 -0400

    documentation

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,17 +4,18 @@
 
 
 class Square:
-    """"""
+    """Class called square for printing hashtag."""
 
     def __init__(self, size=0):
+        """Constructor method."""
         self.size = size
-        """"""
 
     def size(self):
-        """"""
+        """Property getter method."""
         return self.__size
 
     def size(self, value):
+        """Property setter method."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,11 +24,11 @@ class Square:
             self.__size = value
 
     def area(self):
-        """"""
+        """Method to calculate square area."""
         return self.__size ** 2
 
     def my_print(self):
-        """"""
+        """Method to print hashtag."""
         if self.size == 0:
             print("")
         else:
