b8ff6b7bf2cac4bd8536e27c9c7ffd7864dbe83a
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-21 11:13:14 -0400

    doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+"""Class called MyList that inherits list."""
+
+
 class MyList(list):
+    """Print list in ascended order."""
+
     def print_sorted(self):
         ascend_list = sorted(self)
         print(ascend_list)
