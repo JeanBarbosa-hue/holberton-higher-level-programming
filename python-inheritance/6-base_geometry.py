c1ad12102020bf913a2a01a1572e3ad4ab268184
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-21 18:19:15 -0400

    exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Public instance that raises exception."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
