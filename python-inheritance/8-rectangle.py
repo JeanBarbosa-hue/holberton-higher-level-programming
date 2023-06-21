9cba438b6e3d16ab3729224b0c1896b9a8d73adf
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-21 19:17:18 -0400

    rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Public instance that raises exception."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value."""
+        if isinstance(name, str):
+            if type(value) is not int:
+                raise TypeError(f"{name} must be an integer")
+            if value <= 0:
+                raise ValueError(f"{name} must be greater than 0")
+
+
+"""Class called rectangle that inherits from basegeometry."""
+
+
+class Rectangle(BaseGeometry):
+    """child class called rectangle."""
+
+    def __init__(self, width, height):
+        """construtor method."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
