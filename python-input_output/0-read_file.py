d75b8a6e391dc13a1f43171f62cf42674a08fb41
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-22 10:46:56 -0400

    doc updt

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+"""Reads file and prints it stdout."""
+
+
 def read_file(filename=""):
+    """Method to read file."""
     with open(filename, 'r') as file:
         read = file.read()
         print(read)
