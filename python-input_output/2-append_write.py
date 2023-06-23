4b90f62bb78d4cf24b5ee493a64b0c6b632b18fb
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-22 20:23:08 -0400

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""function that appends a string at the end of a text
+file and returns the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """method to append string."""
+    with open(filename, 'a') as file:
+        return (file.write(text))
