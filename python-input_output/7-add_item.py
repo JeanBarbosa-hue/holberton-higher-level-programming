edd10be49d4151dae886b18fbc7132505fc5bd59
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-22 22:15:42 -0400

    add item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""script that add all arguments to python list
+and save them to a file."""
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+try:
+    js_list = load_from_json_file("add_item.json")
+except Exception:
+    js_list = []
+
+for arg in sys.argv[1:]:
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
