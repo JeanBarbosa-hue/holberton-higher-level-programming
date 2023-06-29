72014394683ba5807b091f2e6664f2a47ce67ec8
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-06-29 10:07:45 -0400

    udpt

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,3 +15,35 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    def width(self):
+        """property"""
+        return (self.__width)
+
+    def width(self, value):
+        """setter method"""
+        self.__width = value
+
+    def height(self):
+        """property"""
+        return (self.__height)
+
+    def height(self, value):
+        """setter method"""
+        self.__height = value
+
+    def x(self):
+        """property"""
+        return (self.__x)
+
+    def x(self, value):
+        """setter method"""
+        self.__x = value
+
+    def y(self):
+        """property"""
+        return (self.__y)
+
+    def y(self, value):
+        """setter method"""
+        self.__y = value
