97ce45f04069e5e69c606fa7bf3aebfbf8166289
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-07-31 10:31:05 -0400

    fix?

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,9 @@ def list_states(mysql_username, mysql_password, database_name):
 
     """Connects to MySQL database"""
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=mysql_username, passwd=mysql_password, db=database_name)
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
 
     """Creates cursor to execute SQL queries"""
     cur = db.cursor()
